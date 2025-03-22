c754d14d712462f487a66c353dbebfb6c217d034
RobsonArcoleze <Robson.arcoleze29@gmail.com>
2025-03-21 22:35:42 -0300

    feat: Implementa component ConteudoPricipal
    - Implementa a diretiva v-for;
    - Usando boa pratica de rederização com :key;

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPricipal from './components/ConteudoPrincipal.vue';
 
 </script>
 
 <template>
   <Banner/>
+  <ConteudoPricipal />
+  
 </template>
 
 
